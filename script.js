7a1d7196ed057bc014f011300e01e5e5fb63af97
unknown <dongnhatanhtam01@gmail.com>
2021-03-06 01:57:22 +0700

    còn lỗi do mỗi dot có khi kích thước dot.size < 0

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const WIDTH = 1400
 const HEIGHT = 800
-const PARTICLE_SIZE = 5
+const PARTICLE_SIZE = 8
 const PARTICLE_CHANGE_SIZE_SPD = 0.1
 const PARTICLE_CHANGE_SPD = 0.3
 const ACCELERATION = .1
+
+// dot parameter
+const DOT_CHANGE_SIZE_SPD = .05
+const DOT_CHANGE_ALP_SPD = .05
+
+// toc do ban it nhat
+const PARTICAL_MIN_SPD = 10
+
+// amount of particle in 1 bullet
+NUMBER_PARTICAL_PER_BULLET = 25
 
 class particle {
   constructor(bullet, deg) {
     this.bullet = bullet
     this.ctx = bullet.ctx
+    this.color = this.bullet.color
     this.deg = deg
     this.x = bullet.x
     this.y = bullet.y
     this.size = PARTICLE_SIZE
 
     // toc do phong hat luc khoi tao
-    this.speed = 10
+    this.speed = Math.random() * 4 + PARTICAL_MIN_SPD
     this.speedX = 0
     this.speedY = 0
     this.fallSpeed = 0
+
+    // cham con cua partical
+    this.dots = []
+    // { x: 10, y: 10, alpha: 1, size: 10 }
   }
   update() {
     // ban ra thay doi toc do
@@ -32,7 +47,7 @@ class particle {
 
     // tao random do lon cua dai luong speedX va speedY
     this.speedX = this.speed * Math.cos(this.deg)
-    this.speedY = this.speed * Math.sin(this.deg)
+    this.speedY = this.speed * Math.sin(this.deg) - this.fallSpeed
     // calculate position
     this.x += this.speedX
     this.y += this.speedY
@@ -41,12 +56,37 @@ class particle {
     if (this.size > PARTICLE_CHANGE_SIZE_SPD) {
       this.size -= PARTICLE_CHANGE_SIZE_SPD
     }
+
+    // neu hat con du lon va con xuat hien tren man hinh
+    if (this.size > 0) {
+      this.dots.push({
+        x: this.x,
+        y: this.y,
+        alpha: 1,
+        size: this.size
+      })
+    }
+
+    // lam mo cham con
+    this.dots.forEach(dot => {
+      dot.size -= DOT_CHANGE_SIZE_SPD
+      dot.alpha -= DOT_CHANGE_ALP_SPD
+    }) 
+
   }
   draw() {
-    this.ctx.fillStyle = '#ff0000'
-    this.ctx.beginPath()
-    this.ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
-    this.ctx.fill()
+    // ve them hat cho dep
+    this.dots.forEach(dot => {
+      this.ctx.fillStyle = 'rgba('+this.color+','+dot.alpha+' )'
+      this.ctx.beginPath()
+      this.ctx.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI)
+      this.ctx.fill()
+    })
+
+    // this.ctx.fillStyle = '#ff0000'
+    // this.ctx.beginPath()
+    // this.ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
+    // this.ctx.fill()
   }
 }
 
@@ -64,10 +104,9 @@ class bullet {
     this.particles = []
 
     // 10 particles
-    for (let i = 0; i < 10; i++) {
+    let bulletDeg = Math.PI * 2 / NUMBER_PARTICAL_PER_BULLET
+    for (let i = 0; i < NUMBER_PARTICAL_PER_BULLET; i++) {
       // tinh deg / 1 particle
-      let bulletDeg = Math.PI * 2 / 10
-
       let newParticle = new particle(this, i * bulletDeg)
       this.particles.push(newParticle)
     }
